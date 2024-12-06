1945f65a57667d1078d8296456fe0908ec6bdf77
Abaku009 <164512931+Abaku009@users.noreply.github.com>
2024-12-06 18:37:46 +0100

    Start menu, exit, loves, aszteroida elpusztul

---
+++ Assets/shootDetection.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shootDetection : MonoBehaviour
+{
+    public int health = 3; // Az aszteroida életereje
+    public GameObject explosionEffect; // Robbanás effekt prefab (opcionális)
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Ellenõrizzük, hogy a találkozó objektum a lövedék-e
+        if (other.CompareTag("Bullet"))
+        {
+            Debug.Log("Aszteroida találatot kapott!");
+
+            // Életerõ csökkentése
+            health--;
+
+            // Ha az életerõ 0 vagy kevesebb, az aszteroida felrobban
+            if (health <= 0)
+            {
+                Explode();
+            }
+
+            // Lövedék eltávolítása
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void Explode()
+    {
+        Debug.Log("Aszteroida megsemmisült!");
+
+        // Robbanás effekt létrehozása
+        if (explosionEffect != null)
+        {
+            Instantiate(explosionEffect, transform.position, Quaternion.identity);
+        }
+
+        // Az aszteroida eltávolítása
+        Destroy(gameObject);
+    }
+}
