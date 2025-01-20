c292923c7bd0e1bdef36def1128d8127004453e1
Farkaska02 <farkas.mark.t@gmail.com>
2025-01-20 18:23:01 +0100

    end screen prefabok

--- Assets/Endlevel.cs
+++ Assets/Endlevel.cs
@@ -5,9 +5,32 @@ using UnityEngine;
 public class Endlevel : MonoBehaviour
 {
     public int level;
+    public GameObject Vscreen;
+    public GameObject Lscreen;
+
+    public void LevelFailed()
+    {
+        Showlevelfailedscreen();
+    }
+    public void LevelCompleted()
+    {
+        UnlockNextLevel();
+        Showlevelcompletescreen();
+    }
+    public void Showlevelfailedscreen()
+    {
+        Instantiate(Lscreen);
+    }
+
+
+    public void Showlevelcompletescreen()
+    {
+        Instantiate(Vscreen);
+    }
+
 
     // Update is called once per frame
-    void UnlockNextLevel()
+    public void UnlockNextLevel()
     {
         LevelManager.Instance.UnlockLevel(level+1);
     }
